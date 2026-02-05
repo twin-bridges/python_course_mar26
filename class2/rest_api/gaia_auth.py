0ca985e9de5bdf8d7e08c4a2bab29bd665b2a4fb
Kirk Byers <ktbyers@twb-tech.com>
2026-02-05 13:03:30 -0800

    Gaia auth and saving the session ID

--- class2/rest_api/gaia_auth.py
+++ class2/rest_api/gaia_auth.py
@@ -1,7 +1,9 @@
 import requests
 import os
 import json
+from rich import print
 from dotenv import load_dotenv
+import ipdb # noqa
 
 if __name__ == "__main__":
     host = "chkpnt-pod99.lasthop.io"
@@ -14,7 +16,7 @@ if __name__ == "__main__":
     user = "admin"
     admin_pass = os.environ["CHKP_ADMIN"]
 
-    url = f"{base_url}/{endpoint}"
+    url = f"{base_url}{endpoint}"
     headers = {"Content-Type": "application/json"}
     login_payload = {"user": user, "password": admin_pass}
     ssl_verify = False
@@ -23,3 +25,23 @@ if __name__ == "__main__":
     response = requests.post(
         url, data=json.dumps(login_payload), headers=headers, verify=ssl_verify
     )
+    print(response)
+    print(response.status_code)
+    resp_struct = response.json()
+    session_id = resp_struct["sid"]
+    print(session_id)
+
+    headers["X-chkp-sid"] = session_id
+    print(headers)
+
+    endpoint = "show-version"
+    url = f"{base_url}{endpoint}"
+    payload = {}
+
+    print(url)
+    response = requests.post(
+        url, data=json.dumps(payload), headers=headers, verify=ssl_verify
+    )
+    print(response.json())
+
+    # ipdb.set_trace()
