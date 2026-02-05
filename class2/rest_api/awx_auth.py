26c2884a3043a51b842bfebfa97267a7f97b147c
Kirk Byers <ktbyers@twb-tech.com>
2026-02-05 13:41:18 -0800

    AWX requests script

--- class2/rest_api/awx_auth.py
+++ class2/rest_api/awx_auth.py
@@ -3,19 +3,29 @@ import os
 import ipdb  # noqa
 from dotenv import load_dotenv
 
-awx_host = "54.241.198.61:32309/#/login"
+awx_host = "54.241.198.61"
 port = "32309"
-base_url = f"http://{awx_host}:{port}"
-token_auth_url = f"{base_url}/api/v2/tokens/"
+base_url = f"http://{awx_host}:{port}/api/v2/"
+token_auth_url = f"{base_url}tokens/"
 
 # This looks for a .env file and loads it
 load_dotenv()
 user = "admin"
-admin_pass = os.environ["CHKP_ADMIN"]
+admin_pass = os.environ["AWX_ADMIN"]
 creds = (user, admin_pass)
 
 res = requests.post(
     token_auth_url, auth=creds, json={"description": "Testing auth"}, verify=False
 )
+json_resp = res.json()
+token = json_resp["token"]
+
+headers = {"Authorization": f"Bearer {token}"}
+base_url = f"http://{awx_host}:{port}/api/v2/"
+endpoint = "projects/"
+url = f"{base_url}{endpoint}"
+
+res = requests.get(url, headers=headers, verify=False)
 ipdb.set_trace()
 print(res)
+print(res.json())
