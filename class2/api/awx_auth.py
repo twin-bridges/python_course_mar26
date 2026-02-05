f515e70b151eaee633b7150adc4b1c2458680f76
Kirk Byers <ktbyers@twb-tech.com>
2026-02-05 15:24:51 -0800

    API examples

--- class2/api/awx_auth.py
+++ class2/api/awx_auth.py
@@ -2,11 +2,12 @@ import requests
 import os
 import ipdb  # noqa
 from dotenv import load_dotenv
+from rich import print
 
 awx_host = "54.241.198.61"
 port = "32309"
 base_url = f"http://{awx_host}:{port}/api/v2/"
-token_auth_url = f"{base_url}tokens/"
+url = f"{base_url}tokens/"
 
 # This looks for a .env file and loads it
 load_dotenv()
@@ -14,18 +15,24 @@ user = "admin"
 admin_pass = os.environ["AWX_ADMIN"]
 creds = (user, admin_pass)
 
-res = requests.post(
-    token_auth_url, auth=creds, json={"description": "Testing auth"}, verify=False
-)
+res = requests.post(url, auth=creds, json={"description": "Testing auth"}, verify=False)
 json_resp = res.json()
 token = json_resp["token"]
+print(url)
+print(res)
 
+# Retrieve some data using existing token
 headers = {"Authorization": f"Bearer {token}"}
-base_url = f"http://{awx_host}:{port}/api/v2/"
 endpoint = "projects/"
 url = f"{base_url}{endpoint}"
 
 res = requests.get(url, headers=headers, verify=False)
-ipdb.set_trace()
+print(url)
 print(res)
-print(res.json())
+
+# Delete the token
+endpoint = f"tokens/{token}/"
+url = f"{base_url}{endpoint}"
+requests.delete(url, headers=headers, verify=False)
+print(url)
+print(res)
