783cb308ba5e84733605e776542eb6bac5c18272
Kirk Byers <ktbyers@twb-tech.com>
2026-02-07 08:11:13 -0800

    Gaia Class

--- class3/gaia_class/gaia_class.py
+++ class3/gaia_class/gaia_class.py
@@ -1,10 +1,15 @@
 import requests
 import os
 import json
-
-# from rich import print
+from rich import print
 from dotenv import load_dotenv
 import ipdb  # noqa
+
+
+class GaiaAuthError(Exception):
+    """Exception raised when the session ID is missing or expired."""
+
+    pass
 
 
 class GaiaAPI:
@@ -13,17 +18,36 @@ class GaiaAPI:
         self.username = username
         self.password = password
         self.base_url = f"https://{host}/gaia_api/v{api_version}/"
+        self.headers = {"Content-Type": "application/json"}
         self.ssl_verify = ssl_verify
 
     def login(self):
-        headers = {"Content-Type": "application/json"}
         login_payload = {"user": self.username, "password": self.password}
         url = self.base_url + "login"
         response = requests.post(
-            url, data=json.dumps(login_payload), headers=headers, verify=self.ssl_verify
+            url,
+            data=json.dumps(login_payload),
+            headers=self.headers,
+            verify=self.ssl_verify,
         )
         resp_struct = response.json()
-        self.session_id = resp_struct["sid"]
+        self.headers["X-chkp-sid"] = resp_struct["sid"]
+
+    def call(self, endpoint, payload=None):
+        url = self.base_url + endpoint
+        if payload is None:
+            payload = {}
+        if "X-chkp-sid" not in self.headers:
+            msg = """
+Session ID not set, please call '.login()' method and properly 
+authenticate to the API.
+"""
+            raise GaiaAuthError(msg)
+
+        response = requests.post(
+            url, data=json.dumps(payload), headers=self.headers, verify=self.ssl_verify
+        )
+        return response.json()
 
 
 if __name__ == "__main__":
@@ -36,22 +60,13 @@ if __name__ == "__main__":
     user = "admin"
     admin_pass = os.environ["CHKP_ADMIN"]
 
+    api_client = GaiaAPI(host=host, username=user, password=admin_pass)
+    api_client.login()
+    print(api_client.session_id)
 
-#    headers["X-chkp-sid"] = session_id
-#    print(headers)
-#
-#    #endpoint = "show-version"
-#    endpoint = "show-api-versions"
-#    url = f"{base_url}{endpoint}"
-#    payload = {}
-#
-#    print(url)
-#    response = requests.post(
-#        url, data=json.dumps(payload), headers=headers, verify=ssl_verify
-#    )
-#    print(response.json())
-#    ipdb.set_trace()
-#
+    res = api_client.call(endpoint="show-version")
+    print(res)
+
 #    endpoint = "logout"
 #    url = f"{base_url}{endpoint}"
 #    payload = {}
