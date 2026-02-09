3954b09606de8ae31542aae9e2035229aaaabad5
Kirk Byers <ktbyers@twb-tech.com>
2026-02-09 11:43:14 -0800

    Gaia functions

--- class2/gaia_func/gaia_funcs.py
+++ class2/gaia_func/gaia_funcs.py
@@ -1,13 +1,5 @@
 import requests
-import os
 import json
-from rich import print
-
-from dotenv import load_dotenv
-import ipdb  # noqa
-
-API_VERSION = 1.8
-HEADERS = {"Content-Type": "application/json"}
 
 
 class GaiaAuthError(Exception):
@@ -66,30 +58,3 @@ def logout(url, headers):
     else:
         msg = "Failed to 'logout' from Gaia API"
         raise GaiaLogoutError(msg)
-
-
-if __name__ == "__main__":
-    host = "chkpnt-pod99.lasthop.io"
-    api_version = "1.8"
-    endpoint = "login"
-    base_url = f"https://{host}/gaia_api/v{API_VERSION}/"
-    headers = {"Content-Type": "application/json"}
-
-    # This looks for a .env file and loads it
-    load_dotenv()
-    user = "admin"
-    admin_pass = os.environ["CHKP_ADMIN"]
-
-    url = base_url + "login"
-    session_id = login(url=url, username=user, password=admin_pass)
-    print(session_id)
-
-    headers["X-chkp-sid"] = session_id
-
-    url = base_url + "show-version"
-    res = api_call(url, headers)
-    print(res.json())
-
-    url = base_url + "logout"
-    headers = logout(url, headers)
-    print(headers)
