5c0341fc311a0c6a5dd4eede5c30788de7792ee6
Kirk Byers <ktbyers@twb-tech.com>
2026-02-12 09:24:31 -0800

    Dict examples

---
+++ class1/dict/dict_ex.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import ipdb  # noqa
+from rich import print
+
+ftp_service = {
+    "uid": "97aeb3d0-9aea-11d5-bd16-0090272ccb30",
+    "name": "ftp",
+    "type": "service-tcp",
+    "domain": {
+        "uid": "a0bbbc99-adef-4ef8-bb6d-defdefdefdef",
+        "name": "Check Point Data",
+        "domain-type": "data domain",
+    },
+    "port": "21",
+    "icon": "Protocols/FTP",
+    "color": "forest green",
+}
+ipdb.set_trace()
+print()
