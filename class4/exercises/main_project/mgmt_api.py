7447ea7bfacfaae3ab42ccfc1a9675dc621d55ca
Kirk Byers <ktbyers@twb-tech.com>
2026-02-25 09:14:17 -0800

    main project mgmt api

---
+++ class4/exercises/main_project/mgmt_api.py
@@ -0,0 +1,62 @@
+import os
+import ipdb  # noqa
+from rich import print  # noqa
+from dotenv import load_dotenv
+from cpapi import APIClient, APIClientArgs
+from chkpt_object_funcs import cfg_host_objects, cfg_group_object
+
+
+def gen_host_object(ip_addr):
+    return {
+        "name": ip_addr,
+        "ipv4-address": ip_addr,
+        "color": "black",
+    }
+
+
+def main():
+    host = "chkpnt-pod99.lasthop.io"
+
+    # This looks for a .env file and loads it
+    load_dotenv()
+    username = "admin"
+    password = os.environ["CHKP_ADMIN"]
+
+    api_version = "2"
+    no_ssl_verify = True
+
+    client_args = APIClientArgs(
+        server=host, api_version=api_version, unsafe=no_ssl_verify, context="web_api"
+    )
+
+    with APIClient(client_args) as api_client:
+        api_client.login(username, password)
+
+        with open("blocked_ips.txt") as f:
+            blocked_ips = f.readlines()
+            # strip trailing newline w/ list comprehension
+            blocked_ips = [ip.strip() for ip in blocked_ips]
+
+        # Convert to host object dict using list comprehension
+        blocked_ip_objs = [gen_host_object(ip) for ip in blocked_ips]
+
+        # Configure host objects for blocked IPs
+        cfg_host_objects(api_client, blocked_ip_objs)
+
+        # Retrieve current Blocked IPs group
+        group_name = "Blocked IPs"
+        ipdb.set_trace()
+        api_res = api_client.api_call(command="show-group", payload={"name": group_name})
+        current_blocked_ips = api_res.data["members"]
+        print(current_blocked_ips)
+        
+        blocked_ip_group = {
+            "name": "Blocked IPs",
+            "members": blocked_ips,
+        }
+        cfg_group_object(api_client, blocked_ip_group)
+
+        api_client.api_call(command="publish")
+
+if __name__ == "__main__":
+    main()
