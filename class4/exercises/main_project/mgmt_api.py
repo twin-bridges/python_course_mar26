854d0e622dc4373a67dbc054b897e777ac7fbe77
Kirk Byers <ktbyers@twb-tech.com>
2026-02-25 09:33:00 -0800

    Class4 main project

--- class4/exercises/main_project/mgmt_api.py
+++ class4/exercises/main_project/mgmt_api.py
@@ -1,9 +1,19 @@
 import os
+import sys
 import ipdb  # noqa
 from rich import print  # noqa
 from dotenv import load_dotenv
 from cpapi import APIClient, APIClientArgs
 from chkpt_object_funcs import cfg_host_objects, cfg_group_object
+
+
+def read_blocked_ips_file():
+    # Retrieve the 'new' blocked IPs
+    with open("blocked_ips.txt") as f:
+        new_blocked_ips = f.readlines()
+        # strip trailing newline w/ list comprehension
+        new_blocked_ips = [ip.strip() for ip in new_blocked_ips]
+        return new_blocked_ips
 
 
 def gen_host_object(ip_addr):
@@ -12,6 +22,18 @@ def gen_host_object(ip_addr):
         "ipv4-address": ip_addr,
         "color": "black",
     }
+
+
+def get_current_blocked_ips(api_client, group_name):
+    """Retrieve current Blocked IPs group membership."""
+    current_blocked_ips = []
+    api_res = api_client.api_call(command="show-group", payload={"name": group_name})
+    if api_res.success:
+        current_blocked_ips = api_res.data["members"]
+
+    # Retrieve only names / use list comprehension
+    cur_blocked_ip_names = [bl_obj["name"] for bl_obj in current_blocked_ips]
+    return cur_blocked_ip_names
 
 
 def main():
@@ -32,31 +54,34 @@ def main():
     with APIClient(client_args) as api_client:
         api_client.login(username, password)
 
-        with open("blocked_ips.txt") as f:
-            blocked_ips = f.readlines()
-            # strip trailing newline w/ list comprehension
-            blocked_ips = [ip.strip() for ip in blocked_ips]
+        group_name = "Blocked IPs"
+        current_blocked_ips = get_current_blocked_ips(api_client, group_name)
+        new_blocked_ips = read_blocked_ips_file()
 
         # Convert to host object dict using list comprehension
-        blocked_ip_objs = [gen_host_object(ip) for ip in blocked_ips]
+        blocked_ip_objs = [gen_host_object(ip) for ip in new_blocked_ips]
+
+        # Compare new versus already configured
+        if set(current_blocked_ips) == set(new_blocked_ips):
+            # Nothing to do, current and new already match.
+            sys.exit(0)
+        else:
+            missing_blocked_ips = set(new_blocked_ips) - set(current_blocked_ips)
+            remove_blocked_ips = set(current_blocked_ips) - set(new_blocked_ips)
+            print(f"{missing_blocked_ips=}")
+            print(f"{remove_blocked_ips=}")
 
         # Configure host objects for blocked IPs
         cfg_host_objects(api_client, blocked_ip_objs)
 
-        # Retrieve current Blocked IPs group
-        group_name = "Blocked IPs"
-        ipdb.set_trace()
-        api_res = api_client.api_call(command="show-group", payload={"name": group_name})
-        current_blocked_ips = api_res.data["members"]
-        print(current_blocked_ips)
-        
         blocked_ip_group = {
             "name": "Blocked IPs",
-            "members": blocked_ips,
+            "members": new_blocked_ips,
         }
         cfg_group_object(api_client, blocked_ip_group)
 
         api_client.api_call(command="publish")
+
 
 if __name__ == "__main__":
     main()
