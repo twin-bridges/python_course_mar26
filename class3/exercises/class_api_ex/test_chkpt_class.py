22aeac20397741be49077d76c378e938da337f9f
Kirk Byers <ktbyers@twb-tech.com>
2026-03-03 10:34:48 -0800

    Exercise on classes

--- class3/exercises/class_api_ex/test_chkpt_class.py
+++ class3/exercises/class_api_ex/test_chkpt_class.py
@@ -13,15 +13,48 @@ def gaia_test():
     user = "admin"
     admin_pass = os.environ["CHKP_ADMIN"]
 
-    api_client = ChkptAPI(host=host, username=user, password=admin_pass, mode="gaia_api")
+    api_client = ChkptAPI(
+        host=host, username=user, password=admin_pass, mode="gaia_api"
+    )
     api_client.login()
 
     endpoint = "show-static-routes"
     res = api_client.call(endpoint=endpoint)
+
+    print()
+    print("*" * 40)
+    print(f"--- Gaia Class Test ---")
     print(res.json())
+    print("*" * 40)
+    print()
+
+    api_client.logout()
+
+
+def mgmt_test():
+    host = "chkpnt-pod99.lasthop.io"
+
+    # This looks for a .env file and loads it
+    load_dotenv()
+    user = "admin"
+    admin_pass = os.environ["CHKP_ADMIN"]
+
+    api_client = ChkptAPI(host=host, username=user, password=admin_pass, mode="web_api")
+    api_client.login()
+
+    endpoint = "show-networks"
+    res = api_client.call(endpoint=endpoint)
+
+    print()
+    print("*" * 40)
+    print(f"--- Mgmt Class Test ---")
+    print(res.json())
+    print("*" * 40)
+    print()
 
     api_client.logout()
 
 
 if __name__ == "__main__":
     gaia_test()
+    mgmt_test()
